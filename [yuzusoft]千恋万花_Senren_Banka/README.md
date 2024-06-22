f5b6090d40c6850acc3c2ed8495970df80d97b19
ghastzera <ghast1085654218@163.com>
2024-06-22 17:52:54 +0800

    change files

---
+++ [yuzusoft]千恋万花_Senren_Banka/README.md
@@ -0,0 +1,76 @@
+# 1.解包
+参见``[yuzusoft]星光咖啡馆与死神之蝶_Cafe_Stella``
+# 2.处理
+说话人的对应是: <br>
+>
+>akh 秋穂 <br>
+bsd 武士団 <br>
+chi 千代 <br>
+gda 外国人男性Ａ <br>
+gdb 外国人男性Ｂ <br>
+gen 鞍馬玄十郎 Kurama Genjūrō <br>
+gja 外国人女性Ａ <br>
+gjb 外国人女性Ｂ <br>
+gka 外国人の観光客Ａ <br>
+gkb 外国人の観光客Ｂ <br>
+gkc 外国人の観光客Ｃ <br>
+hha 母Ａ <br>
+hin 比奈実 <br>
+hoa 母親Ａ <br>
+jij 侍女 <br>
+kda 子供Ａ <br>
+kdb 子供Ｂ <br>
+kdc 子供Ｃ <br>
+kdd 子供Ｄ <br>
+kfb 黒服Ｂ <br>
+khc 小春の父親 <br>
+khh 小春の母親 <br>
+kka 客Ａ <br>
+kkb 客Ｂ <br>
+knk 子猫 <br>
+koh 鞍馬小春 Kurama Koharu<br>
+kra 黒服Ａ <br>
+ksa 教師Ａ <br>
+ksb 教師Ｂ <br>
+ksf 高祖父 <br>
+kzt 一豊 <br>
+lem レナ叢雲 <br>
+len レナ (蕾娜·莉希特娜瓦)<br>
+mak 常陸茉子 Hitachi Mako <br>
+mam ママ <br>
+miz 駒川みづは(驹川美津叶) Komakawa Mizuha<br>
+mkh 茉子の母 <br>
+mot 心子 <br>
+msm 娘 <br>
+mur ムラサメ(丛雨) Murasame <br>
+myk 都子 <br>
+nrk 典子 <br>
+nrm 成美 <br>
+occ オッチャン <br>
+oji おじいさん <br>
+onn 怨念 <br>
+osn オッサン <br>
+otk 男 <br>
+oyj オヤジ <br>
+pap パパ <br>
+ren 鞍馬廉太郎 Kurama Rentarō<br>
+rkc 芦花の父親 <br>
+rkh 芦花の母親 <br>
+rok 馬庭芦花 Maniwa Roka <br>
+ruj 老人 <br>
+sgs シゲさん <br>
+snb 産婆 <br>
+sof 祖父 <br>
+srk 白狛 <br>
+tdo 忠興 <br>
+tis 大将 <br>
+tmt 朝武氏 <br>
+tni 店員 <br>
+uts 運転手 <br>
+yas 朝武安晴 Tomotake Yasuharu<br>
+ykh 幸弘 <br>
+yos 朝武芳乃 Tomotake Yoshino<br>
+
+之后就可以按照操作指示制作数据集了。<br>
+处理好的文件可以在本目录下的``scripts_json``文件夹中找到。<br>
+数据集已经上传至huggingface仓库。<https://huggingface.co/datasets/KitsuneX07/Datasets_for_Senren_Banka/tree/main>
---
+++ [yuzusoft]谜语小丑_Riidle_Joker/Text_Extractor_for_Cafe_Stella.py
@@ -0,0 +1,53 @@
+import os
+import glob
+import re
+import json
+
+# 获取scripts文件夹的路径
+scripts_dir = r'F:\unpacked\Cafe_Stella\scn'
+if scripts_dir == '':
+    # 如果没有指定路径，使用当前工作目录下的scripts文件夹
+    scripts_dir = os.path.join(os.getcwd(), 'scripts')
+
+# 获取scripts文件夹下所有txt文件的路径
+json_files = glob.glob(os.path.join(scripts_dir, '*.json'))
+
+# 创建一个新的文件夹用于存放输出文件
+os.makedirs('scripts_json', exist_ok=True)
+
+# 初始化一个空的字典来存储数据
+data = {}
+
+# 遍历所有txt文件
+for json_file in json_files:
+    # 使用UTF-8编码打开并读取文件
+    with open(json_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    # 遍历每一行
+    for i in range(len(lines)):
+        # 查找匹配的字符串
+        # 匹配文件名形如 abc(d)(e)_123456
+        match = re.search(r'"voice": "(([a-zA-Z]+)\d{3}_\d{3})"', lines[i])
+        if match:
+            text_match = re.search(r'「(.*?)」', lines[i-4])
+            if text_match:
+                # 获取匹配到的文本
+                text = text_match.group(1)
+
+                name_match = re.search(r'"name": "(.*?)"', lines[i-3])
+                if name_match:
+                    name = name_match.group(1)
+
+                prefix = match.group(2)
+
+                if prefix not in data:
+                    data[prefix] = []
+                # 将数据添加到列表
+                data[prefix].append({'File': match.group(1), 'Text': text, 'Name': name})
+
+# 遍历数据字典，为每个说话人创建一个JSON文件
+for prefix, prefix_data in data.items():
+    # 将数据写入JSON文件
+    with open(os.path.join('scripts_json', f'{prefix}.json'), 'w', encoding='utf-8') as f:
+        json.dump(prefix_data, f, ensure_ascii=False, indent=4)
